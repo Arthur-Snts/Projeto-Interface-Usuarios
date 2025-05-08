eb512a2555420f61473e077b4a97d1191e177f86
Arthur-Snts <arturpatriciocaico@gamil.com>
2025-05-08 17:16:13 -0300

    teste2

---
+++ testes/aulaDois/src/App.jsx
@@ -0,0 +1,13 @@
+import Escopo from "./componentes/Escopo"
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Escopo />
+    </>
+  )
+}
+
+export default App
---
+++ testes/aulaDois/src/componentes/Escopo.jsx
@@ -0,0 +1,18 @@
+function Escopo () {
+
+    const label = "Clique Aqui"
+
+    return (
+        <>
+            <div>
+                <h2>Texto de Componente</h2>
+            </div>
+            <div>
+                <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+                <button>{label}</button>
+            </div>
+        </>
+    )
+}
+
+export default Escopo
