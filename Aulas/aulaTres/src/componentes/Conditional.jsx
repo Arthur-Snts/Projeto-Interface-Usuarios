eba4f9b7f15c68932de254ca0e62f0d9ef5f7bc2
AlvesArthur1983 <arturpatriciocaico@gmail.com>
2025-05-15 17:53:01 -0300

    Aula3

---
+++ Aulas/aulaTres/src/componentes/Conditional.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react"
+import ChangeValue from "./ChangeValue"
+
+export default Conditional(){
+    let content = <ChangeValue/>
+
+    let [opcao, SetOpcao] = useState(0)
+    
+
+
+}
