486e82a03c2ae3856b43b198408d66946ccd81cf
AlvesArthur1983 <arturpatriciocaico@gmail.com>
2025-07-31 07:36:22 -0300

    Perdi a prova por algo besta ;-; Mas ta aí a correção

--- Provas/Arthur_Alves_Prova02/prova02/src/components/Conexao.jsx
+++ Provas/Arthur_Alves_Prova02/prova02/src/components/Conexao.jsx
@@ -20,36 +20,30 @@ export default function Conexao() {
 
     const filtrarUsuarios = () =>{
         if (id >=1){
-        usuarios.map(usuario=>
-            {if (usuario.id == id){
-                setFiltrado(usuario)
-            }})}
-    }
+        var consulta = usuarios.filter(usuario => usuario.id == id)
+        setFiltrado(consulta[0])
+    }}
     useEffect(()=>{filtrarUsuarios()},[id])
 
 
    
 
     return (
-        <div>
-            {id >=1 &&(
-            <div>
-                <h3>Dados do Usuário de Id: {id}</h3>
-                <ul>
-                    {filtrado}
-                    
-                    <button onClick={() => setCount((count) => count + 1)}>Resgatar Dados</button>
-                </ul>
-                
-            </div>
-            )}
-            {id == 0 &&(
+         <div>
+            <h3>Insira um ID de 1 a 10</h3>
+            <input type="number" value={id} onChange={(e) => setId(Number(e.target.value))} />
+
+            {filtrado && (
                 <div>
-                    <h3>Insira um Id de 1 a 10</h3>
-                    <input type="number" value={id} onChange={(e)=> (setId(e.target.value))} />
+                    <h3>Dados do Usuário de Id: {id}</h3>
+                    <p>{filtrado.name}</p>
+                    <p>{filtrado.username}</p>
+                     <p>{filtrado.address?.city}</p> {/*O React não verificar o valor de address antes de renderizar o filtrado */}
+                    
+                    
+                    <button onClick={() => setCount(count + 1)}>Resgatar Dados</button>
                 </div>
             )}
-            
         </div>
     );
 }
